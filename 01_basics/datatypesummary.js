a7df29626811aa8825636a649d244dda81057773
Pramod Pokharel <pramodpokharel98@gmal.com>
2026-08-28 10:31:54 +0545

    datatype summary

---
+++ 01_basics/datatypesummary.js
@@ -0,0 +1,38 @@
+// # primitive data type
+// 7 types : string(call by value) : ,number:,boolean : ,null ,:undefined: , symbol:, bigint :, 
+
+const score = 100
+const scorevalue = 100.3
+
+const isLoggenIn= false
+const outsideTemp= null
+let userEmail;
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+
+console.log(id === anotherid);
+
+// const bigNumber = 485588583656548n
+
+
+// # refernce type  or non primitive
+// arrays:, objects:, functions:,
+
+const heros=   ["shaktiman","naagraj","doga"]
+let myobj={
+    name:"pramod",
+    age:22,
+}
+const myfunction= function(){ 
+    console.log("hello world")
+    
+}
+console.log(typeof outsideTemp ); //null=>object
+console.log(typeof myfunction);//function=>function
+console.log(typeof heros);//object=>object
+console.log(typeof id);//symbol=>symbol
+console.log(typeof anotherid);//symmbol=>symbol
+console.log(typeof score);  // number=>number
+console.log(typeof scorevalue);//number=>number
+console.log(typeof isLoggenIn); //boolean=>boolean
